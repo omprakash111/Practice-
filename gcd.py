f911df911d72cc4afe097b4e208c9f9d40fc61cd
om <om160prakash@outlook.com>
2023-02-03 13:03:28 +0530

    gcd

---
+++ gcd.py
@@ -0,0 +1,19 @@
+def gcd(m,n):
+    fm=[]
+    for i in range(1,m+1):
+      if(m%i)==0:
+        fm.append(i)
+
+    fn=[]
+    for j in range(1,n+1):
+        if(n%j)==0:
+            fn.append(j)
+    
+    cf=[]
+    for f in fm:
+        if f in fn:
+            cf.append(f)
+    return(cf[-1])
+
+hcf=gcd(14,16)
+print(hcf)
