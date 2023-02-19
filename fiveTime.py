21c6c02704eceb11469f22f9c2adb427b9a4f3eb
om <om160prakash@outlook.com>
2023-02-19 17:25:44 +0530

    print five time a print statement

---
+++ fiveTime.py
@@ -0,0 +1,17 @@
+print('My name is ')
+for i in range(1,5,1):
+    print('om prakash ('+str(i) +') ')
+
+for i in range(5):
+    print('om prakash ('+str(i) +') ')
+
+''' 
+  om prakash (1)
+om prakash (2)
+om prakash (3)
+om prakash (4)
+om prakash (0)
+om prakash (1)
+om prakash (2)
+om prakash (3)
+om prakash (4)        '''
